42826090b979d0f37b5b2a61d9eda3907e1fe3d6
Kevin Capule <kapoolay@gmail.com>
2022-12-07 14:41:40 -0500

    first commit

---
+++ python-refresher/01_variables/code.py
@@ -0,0 +1,32 @@
+"""
+x = 15
+price = 9.99
+
+discount = 0.2
+
+result = price * (1 - discount)
+
+print(result)
+"""
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ String variables
+"""
+name = "rolf"
+
+print(name)
+print(name * 2)
+# print(f"Hello there, {name.title()}!")
+"""
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Changing variable values
+
+a = 25
+b = a  # b will evaluate to 25
+
+print(a)
+print(b)
+
+b = 17
+
+print(a)
+print(b)
